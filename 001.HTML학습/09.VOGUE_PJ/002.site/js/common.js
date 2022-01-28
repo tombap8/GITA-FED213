69a5793dd19615e6b825570fe62f56d29deeb189
Tom B. Parker <tombap8@hotmail.com>
2022-01-28 16:57:42 +0900

    Update common.js

--- 001.HTML학습/09.VOGUE_PJ/002.site/js/common.js
+++ 001.HTML학습/09.VOGUE_PJ/002.site/js/common.js
@@ -6,9 +6,19 @@
 window.addEventListener("scroll",()=>{
 
     // 스크롤 위치값
-    let scTop = this.scrollY;
+    let scTop = document.querySelector("html").scrollTop;
     // scrollY - 세로축 스크롤 위치값 리턴
     // this는 화살표함수에서 window객체를 나타냄
+
+    /************************************************** 
+        [윈도우 세로 스크롤 위치값 가져오는 방법]
+        1. this.scrollY (this키워드가 window의미)
+        2. window.scrollY
+        3. document.scrollingElement.scrollTop
+        4. document.documentElement.scrollTop
+        5. document.querySelector("html").scrollTop
+
+    **************************************************/
 
     console.log("스위:",scTop);
 
