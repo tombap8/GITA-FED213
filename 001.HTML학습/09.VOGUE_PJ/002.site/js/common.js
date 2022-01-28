45d4ff932b25f9d4eef8d1742f5f81845ae80463
Tom B. Parker <tombap8@hotmail.com>
2022-01-28 17:17:58 +0900

    Update common.js

--- 001.HTML학습/09.VOGUE_PJ/002.site/js/common.js
+++ 001.HTML학습/09.VOGUE_PJ/002.site/js/common.js
@@ -6,14 +6,17 @@
 // 스크롤 위치값
 let scTop;
 // 상단영역 - #top
-let top;
+let topA;
 /***********************************/
 
 ///////////// 로드구역 //////////////////////
 window.addEventListener("DOMContentLoaded",
 ()=>{
     console.log("로딩완료!");
-    
+
+    // 변경대상 : 상단영역 - #top (전역변수 top)
+    topA = document.querySelector("#top");
+
 }); //////////// 로드구역 //////////////////
 ///////////////////////////////////////////
 
@@ -22,8 +25,8 @@ window.addEventListener("DOMContentLoaded",
 // window객체에 스크롤이벤트를 걸어준다!(전체스크롤)
 window.addEventListener("scroll",()=>{
 
-    // 스크롤 위치값
-    let scTop = this.scrollY;
+    // 스크롤 위치값(전역변수 scTop)
+    scTop = this.scrollY;
     // scrollY - 세로축 스크롤 위치값 리턴
     // this는 화살표함수에서 window객체를 나타냄
 
@@ -42,10 +45,9 @@ window.addEventListener("scroll",()=>{
 
     **************************************************/
 
-    console.log("스위:",scTop);
+    // console.log("스위:",scTop);
 
-    // 변경대상 : 상단영역 - #top
-    let top = document.querySelector("#top");
+    // 변경대상 : 상단영역 - #top -> 전역변수 topA
 
     //////////////////////////////
     // 상단영역 스크롤 반응하기 ///
@@ -53,12 +55,19 @@ window.addEventListener("scroll",()=>{
 
     // 1. 스크롤위치가 100px이상일때
     if(scTop>=100){
-        top.classList.add("on");
+        topA.classList.add("on");
     } /////////// if ////////////
     // 2. 스크롤위치가 100미만일때
     else{
-        top.classList.remove("on");
+        topA.classList.remove("on");
     } /////////// else ///////////
+
+    /*********************************************** 
+        [ 클래스 관리객체 : classList ]
+        1. 삽입 메서드 : add(클래스명)
+        2. 제거 메서드 : remove(클래스명)
+        3. 삽입/제거 전환 메서드 : toggle(클래스명)
+    ***********************************************/
 
 
 }); ///////////// scroll ////////////////
