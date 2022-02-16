b6968b6540ff8f6c6217451b600de575cd00f218
Tom B. Parker <tombap8@hotmail.com>
2022-02-16 11:20:53 +0900

    ss

--- 001.HTML학습/09.VOGUE_PJ/002.site/js/common.js
+++ 001.HTML학습/09.VOGUE_PJ/002.site/js/common.js
@@ -67,39 +67,44 @@ $(() => {
         - 변경대상: div#cs (cursor를 줄임)
         - 방법: 동적으로 body에 #cs를 추가함
     **************************************/
-    $("body").append('<div id="cs"></div>');
-    // 커서 변수에 할당
-    let cs = $("#cs");
-    // 커서에 css설정
-    cs.css({
-        position: "absolute",
-        top: "0",
-        left: "0",
-        width: "80px",
-        height: "80px",
-        borderRadius: "50%",
-        backgroundColor: "red",
-        transition: ".4s ease-out",
-        mixBlendMode: "exclusion",
-        /* 겹쳐질때 색상변경모드 */
-        zIndex: "9999"
-    }); ////// css /////////
+    // $("body").append('<div id="cs"></div>');
+    // // 커서 변수에 할당
+    // let cs = $("#cs");
+    // // 커서에 css설정
+    // cs.css({
+    //     position: "fixed",
+    //     top: "0",
+    //     left: "0",
+    //     width: "80px",
+    //     height: "80px",
+    //     borderRadius: "50%",
+    //     backgroundColor: "silver",
+    //     transition: ".4s ease-out",
+    //     mixBlendMode: "exclusion",
+    //     /* 겹쳐질때 색상변경모드 */
+    //     zIndex: "9999"
+    // }); ////// css /////////
 
-    /* 
-        mix-blend-mode 속성
-        https://www.w3schools.com/csSref/playdemo.asp?filename=playcss_mix-blend-mode&preval=normal
-    */
+    // /* 
+    //     mix-blend-mode 속성 : 겹쳐져서 색상을 변경하는 속성
+    //     https://www.w3schools.com/csSref/playdemo.asp?filename=playcss_mix-blend-mode&preval=normal
+    // */
 
-    // 윈도우에서 마우스 움직일때 커서 따라다니기!
-    $(window).mousemove((e)=>{
-        // console.log(e.pageX, e.pageY);
-        // 커서의 top, left 위치값 변경
-        cs.css({
-            top: (e.pageY-40) + "px",
-            left: (e.pageX-40) + "px"
-        }); ///// css /////////
+    // // 윈도우에서 마우스 움직일때 커서 따라다니기!
+    // $(window).mousemove((e)=>{
+    //     // console.log(e.pageX, e.pageY);
+    //     // 커서의 top, left 위치값 변경
+    //     cs.css({
+    //         top: (e.pageY-40) + "px",
+    //         left: (e.pageX-40) + "px"
+    //     }); ///// css /////////
 
-    }); /////// mousemove ///////////
+    // }); /////// mousemove ///////////
+
+    // // a요소를 따라다니는 커서 보다 위에오도록
+    // // 포지션을 주고 z-index를 높인다!
+    // // z-index는 포지션이 있어야 적용됨!
+    // $("a").css({position:"relative",zIndex:"99999"})
 
 
 
