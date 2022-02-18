7aef36a3008e4993d375661eb34cd0cc6bd2ebf1
Tom B. Parker <tombap8@hotmail.com>
2022-02-18 13:40:24 +0900

    ss

--- 001.HTML학습/09.VOGUE_PJ/002.site/js/gallery.js
+++ 001.HTML학습/09.VOGUE_PJ/002.site/js/gallery.js
@@ -4,5 +4,14 @@ $(()=>{ ////////// jQB ///////////////////////////
 
     console.log("갤러리 로딩완료!");
 
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+
 
 }); ////////////// jQB ///////////////////////////
