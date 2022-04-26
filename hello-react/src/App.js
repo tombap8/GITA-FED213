b57edd31eacb95273e172106b58fc271e0207200
tombraceparker <tombap8@hotmail.com>
2022-04-26 11:24:15 +0900

    ss

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "리액트양!";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* 주석이양*/}
+      <div
+        className="react"
+        // 여기주석
+      >
+        {name}
+      </div>
+      <input />
+    </>
   );
 }
-
 export default App;
