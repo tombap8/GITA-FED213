63041e5baadf870cd3d4445c073383f70d7770d6
tombraceparker <tombap8@hotmail.com>
2022-06-12 22:07:47 +0900

    ss

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,18 +1,7 @@
-import "./App.css";
+import Counter from "./Counter";
 
-function App() {
-  const name = "리액트양!";
-  return (
-    <>
-      {/* 주석이양*/}
-      <div
-        className="react"
-        // 여기주석
-      >
-        {name}
-      </div>
-      <input />
-    </>
-  );
-}
+const App = () => {
+  return <Counter />
+};
+
 export default App;
